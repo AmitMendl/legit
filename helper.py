319fb2cf676e5d3467843774cc4f0ef3a2ea839e
AmitMendl <amit@mendelevitch@gmail.com>
2022-07-02 16:54:45 +0300

    GoogleCTF2022

--- helper.py
+++ helper.py
@@ -5,14 +5,23 @@ import re
 import subprocess
 import urllib.parse
 
-def show_file(real_filepath):
-  print(os.path.join('/tmp/https%3A%2F%2Fgithub%2ecom%2FAmitMendl%2Flegit%2egit', real_filepath))
-  result = subprocess.run(["cat", os.path.join("/tmp/https%3A%2F%2Fgithub%2ecom%2FAmitMendl%2Flegit%2egit/", real_filepath)], capture_output=True)
-  # if result.returncode != 0:
-  #   print("Error while retrieving file content.")
-  #   return
+_REPO_DIR = '/mnt/c/Users/amitm/Downloads/googlectf2022/LEGIT/legit'
+
+def show_file(filepath):
+      
+  real_filepath = os.path.realpath(os.path.join(_REPO_DIR, filepath))
+  print(_REPO_DIR + filepath)
+  print(os.path.join(_REPO_DIR, filepath))
+  print(real_filepath)
+  if _REPO_DIR != os.path.commonpath((_REPO_DIR, real_filepath)):
+    print("Hacker detected!")
+    return    
+  result = subprocess.run(["cat", os.path.join(_REPO_DIR, real_filepath)], capture_output=True)
+  if result.returncode != 0:
+    print("Error while retrieving file content.")
+    return
   print(result)
   print(result.stdout.decode())
 
 if __name__ == '__main__':
-  show_file('/flag')
+  show_file('test/file')
